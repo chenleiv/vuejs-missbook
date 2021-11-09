03b81684c7f0000a19110a8247961ac94f45e047
chenleiv <chenleiv@protonmail.com>
2021-11-09 17:35:14 +0200

    fix img for git

--- js/pages/about-page.cmp.js
+++ js/pages/about-page.cmp.js
@@ -3,7 +3,7 @@
 export default {
   template: `
         <section class="about-page app-main">
-            <img class="about-img" src='./../img/about.png'/>
+            <img class="about-img" src='../img/about.png'/>
             <h3 ref="header">About us...</h3>
             <p>Book Depository (bookdepository.com) is a leading international book retailer with a unique offer -- over 20 million books and free delivery worldwide (with no minimum spend).
 We ship thousands of books every day from our fulfillment centres in Gloucester, United Kingdom, and Melbourne, Australia, to more than 130 countries across the world -- displaying prices in 37 different local currencies. </p>
@@ -12,7 +12,7 @@ We ship thousands of books every day from our fulfillment centres in Gloucester,
         </section>
     `,
   methods: {
-    callBus() {},
+    callBus() { },
   },
   created() {
     console.log('Created');
--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -1,9 +1,9 @@
 export default {
-  template: `
+    template: `
         <section class="home-page app-main">
-        <img class="about-img" src='./../img/home-page-sale.png'/>
+        <img class="about-img" src='../img/home-page-sale.png'/>
   
-            <h3 ref="header">Welcom to our store ! 
+            <h3 ref="header">Welcome to our store ! 
                 special prices waiting for you in the books section
             </h3>
         </section>
